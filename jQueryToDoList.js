d05b2dd701716336a21601d1f8174b1ec14a1e3e
Reetika <chauhanreetika45@gmail.com>
2023-11-16 23:05:50 -0800

    new element delete, toggle not working

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,10 +1,21 @@
 /**
  * Toggles "done" class on <li> element
  */
+$(document).ready(function (){
+$('li').on('click',function(e) {
+  let $this = $(this);
+  $(this).toggleClass('done');
+  
+})
+
 
 /**
  * Delete element when delete link clicked
  */
+$('.delete').on('click',function(e){
+  let $this = $(this);
+  $(this).parent().remove();
+})
 
 /**
  * Adds new list item to <ul>
@@ -12,8 +23,31 @@
 const addListItem = function(e) {
   e.preventDefault();
   const text = $('input').val();
+  const $newliEl = $('<li>')
+  const $span = $('<span>')
+  $span.text(text)
+  const $newanchor = $('<a>')
+  $newanchor.text('Delete')
+  $newanchor.addClass('delete')
+  $newliEl.append($span)
+  $newliEl.append($newanchor)
+  $('ul').append($newliEl)
+  $('.delete').on('click',function(e){
+    let $this = $(this);
+    $(this).parent().remove();
+  })
+  $('li').on('click',function(e) {
+    let $this = $(this);
+    $(this).toggleClass('done');
+    
+  })
+  
 
   // rest here...
 };
 
 // add listener for add
+$('.add-item').on('click',function(e){
+  addListItem(e)
+})
+})

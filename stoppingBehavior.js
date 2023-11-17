c06b1c01f6a71d1815efb6d381f89d3687ddc5bd
Reetika <chauhanreetika45@gmail.com>
2023-11-16 22:19:11 -0800

    implemented carprototype,jquerydomcrud,stooping behaviour

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,21 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
-
+$(document).ready(function (){
+  const $anchor = $('#more-info')
+  $anchor.attr('href',"")
+  $anchor.on('click', () =>{
+    let text = `Here's some info`
+    alert(text)
+  })
+    
+})
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+$barkbutton = $('#dog')
+$barkbutton.on('click',function (e) {
+  e.stopPropagation()
+})
+$barkbutton.on('click',() =>{
+  alert("Bow wow!")
+})
